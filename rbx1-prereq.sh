93eb2353ff01e44290c8e31304a44c19f1373914
lennonwoo <cccmorin@gmail.com>
2017-12-05 15:01:11 +0800

    kinetic prereq

--- rbx1-prereq.sh
+++ rbx1-prereq.sh
@@ -2,18 +2,30 @@
 
 # Install the prerequisites for the ROS By Example code, Volume 1
 
-sudo apt-get install ros-indigo-turtlebot-bringup \
-ros-indigo-turtlebot-create-desktop ros-indigo-openni-* \
-ros-indigo-openni2-* ros-indigo-freenect-* ros-indigo-usb-cam \
-ros-indigo-laser-* ros-indigo-hokuyo-node \
-ros-indigo-audio-common gstreamer0.10-pocketsphinx \
-ros-indigo-pocketsphinx ros-indigo-slam-gmapping \
-ros-indigo-joystick-drivers python-rosinstall \
-ros-indigo-orocos-kdl ros-indigo-python-orocos-kdl \
-python-setuptools ros-indigo-dynamixel-motor-* \
-libopencv-dev python-opencv ros-indigo-vision-opencv \
-ros-indigo-depthimage-to-laserscan ros-indigo-arbotix-* \
-ros-indigo-turtlebot-teleop ros-indigo-move-base \
-ros-indigo-map-server ros-indigo-fake-localization \
-ros-indigo-amcl git subversion mercurial
+sudo apt-get install ros-kinetic-turtlebot-bringup \
+ros-kinetic-turtlebot-create ros-kinetic-openni-* \
+ros-kinetic-openni2-* ros-kinetic-freenect-* ros-kinetic-usb-cam \
+ros-kinetic-laser-* \
+ros-kinetic-slam-gmapping \
+ros-kinetic-joystick-drivers python-rosinstall \
+ros-kinetic-orocos-kdl ros-kinetic-python-orocos-kdl \
+python-setuptools ros-kinetic-dynamixel-motor-* \
+python-opencv ros-kinetic-vision-opencv \
+ros-kinetic-depthimage-to-laserscan \
+ros-kinetic-turtlebot-teleop ros-kinetic-move-base \
+ros-kinetic-map-server ros-kinetic-fake-localization \
+ros-kinetic-amcl git
+# use git clone source code then catkin_make
+# ros-kinetic-arbotix-* \
 
+# audio see you later
+# ros-kinetic-audio-common ros-kinetic-pocketsphinx gstreamer0.10-pocketsphinx \
+
+# version control see you later
+# subversion mercurial
+
+# don't have kokuyo
+# ros-kinetic-hokuyo-node
+
+# TX2 have its own version
+# libopencv-dev
